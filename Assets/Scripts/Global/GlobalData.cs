07f46d24bdefde2b8b1e561b06d3cd5d6234cb5a
NoahTaylorDev <noaht5.1999@gmail.com>
2025-11-22 02:38:54 -0500

    Feat: Add incremental increases to the health and damage upgrade menu. Made sure it updated globaly.

--- Assets/Scripts/Global/GlobalData.cs
+++ Assets/Scripts/Global/GlobalData.cs
@@ -5,13 +5,24 @@ public class GlobalData : ScriptableObject
 {
     public int boombucks;
     public float playerHealth;
-    public float playerDamageBonus;
+
+    public float playerHealthIncrement;
+    public float playerDamage;
+
+    public float playerDamageIncrement;
+
+    public int healthCost;
+    public int damageCost;
 
     public GlobalData()
     {
         boombucks = 10;
-        playerHealth = 10f;
-        playerDamageBonus = 2f;
+        playerHealth = 50f;
+        playerHealthIncrement = 10f;
+        playerDamage = 4f;
+        playerDamageIncrement = 1f;
+        healthCost = 100;
+        damageCost = 100;
     }
 
 
@@ -30,5 +41,16 @@ public class GlobalData : ScriptableObject
         return false;
     }
 
+    public void IncrementHealth()
+    {
+        healthCost = (int)(healthCost * 1.2f);
+        playerHealth += playerHealthIncrement;
+    }
+
+    public void IncrementDamage()
+    {
+        damageCost = (int)(damageCost * 1.2f);
+        playerDamage += playerDamageIncrement;
+    }
 
 }
--- Assets/Scripts/UI/ProgressionMenuController.cs
+++ Assets/Scripts/UI/ProgressionMenuController.cs
@@ -14,21 +14,30 @@ public class ProgressionMenuController : MonoBehaviour
     private Button buyHealth;
     [SerializeField]
     private TMP_Text boombucksText;
-    private int damageCost = 100;
-    private int healthCost = 100;
 
+    [SerializeField]
     private TMP_Text healthText;
+
+    [SerializeField]
+    private TMP_Text damageText;
+    [SerializeField]
+    private TMP_Text healthInfoText;
+    [SerializeField]
+    private TMP_Text damageInfoText;
+
+    
 
     [SerializeField] public GlobalData globalData;
 
-    public int Boombucks => globalData.boombucks;
-
     void Start()
     {
-        healthText = GetComponentInChildren<TMP_Text>();
-        healthText.text = $"{healthCost}";
-        boombucksText.text = $"{Boombucks}";
         SaveSystem.LoadGame();
+        globalData.AddBoombucks(500);
+        UpdateHealthText();
+        UpdateDamageText();
+        UpdateBoomBucksText();
+        UpdateHealthInfo();
+        UpdateDamageInfo();
     }
 
     public void CloseClick()
@@ -36,19 +45,52 @@ public class ProgressionMenuController : MonoBehaviour
         this.GameObject().SetActive(false);
     }
 
+    public void UpdateHealthInfo()
+    {
+        healthInfoText.text = $"Health: {globalData.playerHealth} => {globalData.playerHealth + globalData.playerHealthIncrement}";
+    }
+
+    public void UpdateDamageInfo()
+    {
+        damageInfoText.text = $"Damage: {globalData.playerDamage} => {globalData.playerDamage + globalData.playerDamageIncrement}";
+    }
+
+    public void UpdateHealthText()
+    {
+        healthText.text = $"{globalData.healthCost}";
+    }
+
+    public void UpdateDamageText()
+    {
+        damageText.text = $"{globalData.damageCost}";
+    }
+
+    public void UpdateBoomBucksText()
+    {
+        boombucksText.text = $"{globalData.boombucks}";
+    }
+
+
+
     public void BuyDamageClick()
     {
-        
+        if (globalData.SpendBoombucks(globalData.damageCost))
+        {
+            globalData.IncrementDamage();
+            UpdateDamageText();
+            UpdateBoomBucksText();
+            UpdateDamageInfo();
+        } 
     }
 
     public void BuyHealthClick()
     {
-        if (globalData.SpendBoombucks(healthCost))
+        if (globalData.SpendBoombucks(globalData.healthCost))
         {
-            healthCost += 100;
-        }
-        
-        healthText.text = $"{healthCost}";
-        boombucksText.text = $"{Boombucks}";
+            globalData.IncrementHealth();
+            UpdateHealthText();
+            UpdateBoomBucksText();
+            UpdateHealthInfo();
+        } 
     }
 }
